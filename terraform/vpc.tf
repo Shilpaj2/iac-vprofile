304d4078331949c93d1ee482d0101cf9c71e8cb3
Shilpaj2 <92943901+Shilpaj2@users.noreply.github.com>
2024-10-14 15:59:03 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,26 +1,14 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-  name = "vprofile-eks"
-
-  cidr = "172.20.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
-  public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
+  tags = {
+    Name = "main"
   }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
+  tags = {
+    Name = "igw"
   }
 }
